1e62a5f9491bb195365e4481214bc6dca249b8f2
Jon Edwards <jkedwards@me.com>
2015-04-13 22:17:22 -0700

    basic functionality implemented

--- client/index.js
+++ client/index.js
@@ -2,8 +2,6 @@
 
 $(document).ready(init);
 
-var firstTowerClicked;
-
 function init(){
   $('#start').click(startGame);
   $('#t1').click(moveHandler);
@@ -12,24 +10,36 @@ function init(){
 }
 
 function startGame(){
-  $('.tower').empty();
   console.log('start game');
   drawCylinders();
 }
 
 function drawCylinders(){
+  var colorArray = ['', 'red', 'green', 'blue', 'yellow', 'purple', 'orange', 'gray', 'tan'];
+  $('.cylinderwrapper').empty();
   var numberOfCylinders = $('#cylinders').val();
   for(var i = 1; i <= numberOfCylinders; i++){
     var $div = $('<div>');
     var width = (i / numberOfCylinders) * 90 + '%';
     $div.text(i);
     $div.addClass('cylinder');
-    $div.css('width', width);
-    $('#t1').append($div);
+    $div.css({'width': width, 'background-color': colorArray[i]});
+    $('#t1').find('.cylinderwrapper').append($div);
   }
 }
 
 function moveHandler(){
-  firstTowerClicked = $(this);
-  var sizeOfCylinderToMove = $(this).children().first().text();
+  if($('.selected').length === 0){
+    $(this).addClass('selected');
+  }
+  else{
+    var sizeOfCylinderToMove = $('.selected').find('.cylinderwrapper').children().first().text();
+    var sizeOfTopCylinderOnTowerToMoveTo = $(this).find('.cylinderwrapper').children().first().text();
+    console.log('first cylinder', sizeOfCylinderToMove, 'second cylinder', sizeOfTopCylinderOnTowerToMoveTo);
+    if((sizeOfCylinderToMove < sizeOfTopCylinderOnTowerToMoveTo) || (sizeOfTopCylinderOnTowerToMoveTo === '')){
+      var detachedTag = $('.selected').find('.cylinderwrapper').children().first().detach();
+      $(this).find('.cylinderwrapper').prepend(detachedTag);
+    }
+    $('.selected').removeClass('selected');
+  }
 }
