e6dcd4db92c1f400afd310d3cfd1a76e248574a4
Jon Edwards <jkedwards@me.com>
2015-04-13 18:25:14 -0700

    basic framework completed

--- client/index.js
+++ client/index.js
@@ -1,2 +1,35 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+var firstTowerClicked;
+
+function init(){
+  $('#start').click(startGame);
+  $('#t1').click(moveHandler);
+  $('#t2').click(moveHandler);
+  $('#t3').click(moveHandler);
+}
+
+function startGame(){
+  $('.tower').empty();
+  console.log('start game');
+  drawCylinders();
+}
+
+function drawCylinders(){
+  var numberOfCylinders = $('#cylinders').val();
+  for(var i = 1; i <= numberOfCylinders; i++){
+    var $div = $('<div>');
+    var width = (i / numberOfCylinders) * 90 + '%';
+    $div.text(i);
+    $div.addClass('cylinder');
+    $div.css('width', width);
+    $('#t1').append($div);
+  }
+}
+
+function moveHandler(){
+  firstTowerClicked = $(this);
+  var sizeOfCylinderToMove = $(this).children().first().text();
+}
